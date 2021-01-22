d2c7493327b89e1d155bc9435a20d16adf7f9438
isacfarias <isac.farias.21@gmail.com>
2021-01-21 22:06:43 -0400

    simplificado formataçaõ de data no log

--- logger-lib/src/main/java/logger/console/ConsoleLogPrinter.java
+++ logger-lib/src/main/java/logger/console/ConsoleLogPrinter.java
@@ -8,15 +8,11 @@ import logger.interfaces.LogPrinter;
 public class ConsoleLogPrinter implements LogPrinter {
 	
 	public void print(String log) {
-		
-		SimpleDateFormat formt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		
-		System.out.println(formt.format(new Date()) +"- INFO - "+log);
+		System.out.println(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()) +"- INFO - "+log);
 	}
 	
 	public void printError(String log) {
-		SimpleDateFormat formt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		System.err.print(formt.format(new Date()) +"- ERROR - "+log);
+		System.err.println(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()) +"- ERROR - "+log);
 	}
 
 }
