5ff1d09eff5e16411bb5f625dccb608f53c5eef2
isacfarias <isac.farias.21@gmail.com>
2021-01-25 21:22:48 -0400

    add classe e arquivos xml usados para exemplo

---
+++ contato-sem-padrao-proxy/src/main/java/contato/ContatoXML.java
@@ -0,0 +1,27 @@
+package contato;
+
+public class ContatosXML implements Contatos {
+
+	private Map<String, String> contatosEmCache = new HashMap<>();
+	
+	@SuppressWarnings("unchecked")
+    public ContatosXML(String... nomesArquivos) {
+		XStream xstream = new XStream();
+	    xstream.alias("contatos", List.class);
+	    xstream.alias("contato", Contato.class);
+	    
+	    for (String nomeArquivo : nomesArquivos) {
+	    	System.out.println("Carregando arquivo: " + nomeArquivo);
+	    	List<Contato> contatosCarregados = (List<Contato>) xstream.fromXML(this.getClass().getResource("/" + nomeArquivo));
+	    	for (Contato contato : contatosCarregados) {
+	    		contatosEmCache.put(contato.getEmail(), contato.getNome());
+	    	}
+	    }
+	}
+	
+	@Override
+    public String buscarPor(String email) {
+	    return contatosEmCache.get(email);
+    }
+
+}
