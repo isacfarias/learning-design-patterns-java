10923a67cdf6b06d9db4645e75c69b84f501b988
isacfarias <isac.farias.21@gmail.com>
2021-01-25 21:38:48 -0400

    add metodo de execuçao

--- contato-sem-padrao-proxy/src/main/java/BuscaContato.java
+++ contato-sem-padrao-proxy/src/main/java/BuscaContato.java
@@ -1,4 +1,12 @@
+import contato.ContatosXML;
+import repository.Contatos;
 
 public class BuscaContato {
 
+	public static void main(String[] args) {
+		Contatos contato = new  ContatosXML("contatos1.xml", "contatos2.xml");
+		String nome = contato.buscarPor("joao@email.com");
+		System.out.println(nome);
+	}
+	
 }
