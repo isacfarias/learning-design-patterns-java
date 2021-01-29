b7451ab7d9cae7a69008e52b7fc70e33ebcc2487
isacfarias <isac.farias.21@gmail.com>
2021-01-28 22:59:20 -0400

    arquivos extras

--- maladireta-cliente/src/main/java/com/br/maladireta/xml/ContatosXML.java
+++ maladireta-cliente/src/main/java/com/br/maladireta/xml/ContatosXML.java
@@ -1,18 +1,11 @@
 package com.br.maladireta.xml;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URI;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.br.maladireta.model.Contato;
 import com.br.maladireta.repository.Contatos;
 import com.thoughtworks.xstream.XStream;
-
-import au.com.bytecode.opencsv.CSVReader;
 
 public class ContatosXML implements Contatos{
 	
@@ -24,6 +17,7 @@ public class ContatosXML implements Contatos{
 		this.nomeArquivo = nomeArquivo;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Contato> todos() {
 		XStream xstream = new XStream();
