0eddf44df6163196fa14641c86baa0413adbe555
isacfarias <isac.farias.21@gmail.com>
2021-01-30 20:51:31 -0400

    implementação de injecção de dependencia com weld

--- dependency-injection/src/main/java/com/br/farias/Principal.java
+++ dependency-injection/src/main/java/com/br/farias/Principal.java
@@ -6,11 +6,12 @@ import java.util.Date;
 import com.br.farias.bean.PedidoVendaService;
 import com.br.farias.bean.impl.PedidoVendaServiceImpl;
 import com.br.farias.model.PedidoVenda;
+import com.br.farias.util.weld.WeldContext;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		PedidoVendaService pedidoVendaService = new PedidoVendaServiceImpl();
+		PedidoVendaService pedidoVendaService = WeldContext.INSTANCE.getBean(PedidoVendaServiceImpl.class);
 		PedidoVenda pedido = new PedidoVenda("sabonete", new BigDecimal("3.0"), new BigDecimal("0.05"), new Date());
 		pedidoVendaService.salvar(pedido);
 
--- dependency-injection/src/main/java/com/br/farias/bean/impl/PedidoVendaServiceImpl.java
+++ dependency-injection/src/main/java/com/br/farias/bean/impl/PedidoVendaServiceImpl.java
@@ -3,23 +3,21 @@ package com.br.farias.bean.impl;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.inject.Inject;
+
 import com.br.farias.bean.CalculaImpostoService;
 import com.br.farias.bean.PedidoVendaService;
 import com.br.farias.model.PedidoVenda;
 import com.br.farias.repository.PedidosVendas;
-import com.br.farias.repository.xml.XMLPedidoVendaRepository;
 
 public class PedidoVendaServiceImpl implements PedidoVendaService {
 	
+	@Inject
 	private PedidosVendas pedidosVendas;
 
+	@Inject
 	private CalculaImpostoService calculaImposto;
-	
-	public PedidoVendaServiceImpl() {
-		this.pedidosVendas = new XMLPedidoVendaRepository();
-		this.calculaImposto = new CalculaImpostoImpl();
-	}
-	
+
 	@Override
 	public void salvar(PedidoVenda pedidoVenda) {
 		pedidoVenda.setData(new Date());
